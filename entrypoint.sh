07c34a73391d343a37160e933aa23c2641037b1f
LiHang <1329075357@qq.com>
2022-09-19 17:31:53 +0800

    feat: fix bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,44 @@
 #!/bin/bash
+set -e
+reset_iptables(){
+    iptables -P INPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -t nat -F
+    iptables -t mangle -F
+    iptables -F
+    iptables -X
+}
 
-ip rule add fwmark 0x1 lookup 100
-ip route add local default dev lo table 100
-iptables -t mangle -N clash
-iptables -t mangle -A clash -d 0.0.0.0/8 -j RETURN
-iptables -t mangle -A clash -d 10.0.0.0/8 -j RETURN
-iptables -t mangle -A clash -d 127.0.0.0/8 -j RETURN
-iptables -t mangle -A clash -d 169.254.0.0/16 -j RETURN
-iptables -t mangle -A clash -d 172.16.0.0/12 -j RETURN
-iptables -t mangle -A clash -d 192.168.0.0/16 -j RETURN
-iptables -t mangle -A clash -d 224.0.0.0/4 -j RETURN
-iptables -t mangle -A clash -d 240.0.0.0/4 -j RETURN
-iptables -t mangle -A clash -p udp --dport 53 -j RETURN
-iptables -t mangle -A clash -p tcp -j TPROXY --on-port 7893 --tproxy-mark 0x1
-iptables -t mangle -A clash -p udp -j TPROXY --on-port 7893 --tproxy-mark 0x1
-iptables -t mangle -A PREROUTING -p tcp -j clash
-iptables -t mangle -A PREROUTING -p udp -j clash
-iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+set_clash_iptables(){
+    # 在 nat 表中创建新链
+    iptables -t nat -N CLASHRULE
 
+    iptables -t nat -A CLASHRULE -p tcp --dport 1905 -j RETURN
+
+    iptables -t nat -A CLASHRULE -d 0.0.0.0/8 -j RETURN
+    iptables -t nat -A CLASHRULE -d 10.0.0.0/8 -j RETURN
+    iptables -t nat -A CLASHRULE -d 127.0.0.0/8 -j RETURN
+    iptables -t nat -A CLASHRULE -d 169.254.0.0/16 -j RETURN
+    iptables -t nat -A CLASHRULE -d 172.16.0.0/12 -j RETURN
+    iptables -t nat -A CLASHRULE -d 192.168.0.0/16 -j RETURN
+    iptables -t nat -A CLASHRULE -d 224.0.0.0/4 -j RETURN
+    iptables -t nat -A CLASHRULE -d 240.0.0.0/4 -j RETURN
+    iptables -t nat -A CLASHRULE -p tcp -j REDIRECT --to-ports 7892
+
+    #拦截 dns 请求并且转发!
+    iptables -t nat -A PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 53
+    iptables -t nat -A PREROUTING -p tcp --dport 53 -j REDIRECT --to-ports 53
+
+    # 在 PREROUTING 链前插入 CLASHRULE 链,使其生效
+    iptables -t nat -A PREROUTING -p tcp -j CLASHRULE
+}
+
+reset_iptables
+set_clash_iptables
+
+#开启转发
+echo "1" > /proc/sys/net/ipv4/ip_forward
+ip addr
 exec "$@"
+
